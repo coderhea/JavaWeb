875cc7195251c26b2e72dc1694b1b2d094d488e8
Jiyeon Rhea Park <rheazen@gmail.com>
2020-03-16 02:37:57 +0900

    Create LogoutCon.java
    sessionPjt/src/com.servlet/LogoutCon.java

---
+++ JSP/session/LogoutCon.java
@@ -0,0 +1,33 @@
+package com.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogoutCon
+ */
+@WebServlet("/logoutCon")
+public class LogoutCon extends HttpServlet {
+	//logout request.getSession.invalidate, redirect to login
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.invalidate();
+		
+		response.sendRedirect("login.jsp"); 
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
